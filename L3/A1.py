f2427efc3b4053c5004c62471179488718cced15
SAY-orignals <sunita.15.07.1985@gmail.com>
2026-01-13 20:13:47 +0300

    Delete L3 directory

--- L3/A1.py
+++
@@ -1,9 +0,0 @@
-with open('Codingal.txt', 'w') as f1:
-    f1.write("My name is Sarthak and I'm 11 years-old")
-f1.close()
-with open('Codingal.txt', 'r') as f2:
-    lines = f2.readlines()
-    for line in lines:
-        word = line.split()
-        print(word)
-f2.close()
