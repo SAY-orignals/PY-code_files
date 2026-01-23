5dc35448ce9481efb8c92c24ab1acca61c0f297c
SAY-orignals <sunita.15.07.1985@gmail.com>
2026-01-23 12:42:20 +0300

    Add files via upload

--- A1.py
+++ A1.py
@@ -1,10 +1,18 @@
-print("increasing pattern: ")
-for r in range(1,9):
-    for c in range(r):
-        print('*',end=' ')
-    print(' ')
-print('decreasing pattern: ')
-for r in range(9,1,-1):
-    for c in range(r):
-        print('*',end=' ')
-    print(' ')
+def fib(nterm):
+    n1,n2=0,1
+    count = 0
+    while count<nterm:
+        print(n1)
+        nth = n1 + n2
+        n1=n2
+        n2=nth
+        count+=1
+nterm = int(input("Enter a term: "))
+if nterm<=0:
+    print("Please enter a natural number!")
+elif nterm==1:
+    print("Fibonacci series until ",nterm,":")
+    fib(nterm)
+else:
+    print("Fibonacci series untill ",nterm,":")
+    fib(nterm)
